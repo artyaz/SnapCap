3ddb1b01437ea098e2df346edf463de0a3343b4a
artyaz <achmilenko58@gmail.com>
2023-02-28 10:21:44 +0200

    Initial Commit

---
+++ Aplpha2/Aplpha2App.swift
@@ -0,0 +1,17 @@
+//
+//  Aplpha2App.swift
+//  Aplpha2
+//
+//  Created by Артем Чмиленко on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Aplpha2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aplpha2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Aplpha2
+//
+//  Created by Артем Чмиленко on 28.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
